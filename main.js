e76673ae7b98555a284d3277a688e6be234e8f06
Magdalena Falkowska-Legieć <magda.f41@gmail.com>
2020-08-06 21:58:00 +0200

    zadanie_29

---
+++ main.js
@@ -0,0 +1,11 @@
+let firstNumber = 5;
+let secondNumber = 7;
+
+
+function sum(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+let suma = sum(firstNumber, secondNumber)
+console.log(suma);
+
